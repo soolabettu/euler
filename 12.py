2b2fdc1a4c0dbf911cd36fdcdd161dc77da9e74e
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-01-04 12:51:26 -0500

    problem solved

---
+++ 12.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+import math
+
+seq = 1
+i = 2
+while True:
+    seq = seq + i
+    i = i + 1
+    count = 0
+    for j in range(1, int(math.sqrt(seq)) + 1):
+        if seq % j == 0:
+            count += 2
+
+    if count > 500:
+        print(seq)
+        break
+
+
+
