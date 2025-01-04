bcb05939658da852adf05c66e754196f4cfb591c
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-01-04 11:38:12 -0500

    problem solved

---
+++ 8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+var = input()
+candidate = ''
+ans = 0
+for i in var:
+    candidate += i
+    if i == '0':
+        candidate = ''
+
+    if len(candidate) == 13:
+        temp_ans = 1
+        for z in candidate:
+            temp_ans = temp_ans * int(z)
+
+        ans = max(ans, temp_ans)
+        candidate = candidate[1:]
+        
+
+print(ans)
+        
