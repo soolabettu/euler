cc3dad5ef28539108e2adab972370ead8f81648c
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-01-05 22:00:50 -0500

    problem solved

---
+++ 22.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+import math
+import time
+from collections import defaultdict
+import sympy    
+
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+
+start = time.time()
+
+names = input().split(",")
+
+ans = 0
+sorted_names = sorted(names)
+for i in range(len(sorted_names)):
+    temp = 0
+    stripped_word = sorted_names[i].strip('"')
+    for j in stripped_word:
+        temp += ord(j) - ord("A") + 1
+
+    ans += temp * (i + 1)    
+
+print(ans)
+end = time.time()
+elapsed = end - start
+print(f"Program took {elapsed:.2f} seconds to run")
+
+
