6469077c4bc68437858ff4069f39d97f1fdd6790
Siddharth Velankar <Siddharth.Velankar@dell.com>
2025-01-22 14:56:29 -0500

    Optimize 39.py

--- 39.py
+++ 39.py
@@ -5,17 +5,23 @@ from collections import defaultdict
 my_dict = defaultdict(int)
 
 def solve():
+    ans = 0
     for i in range(1, 1001):
         for j in range(i+1, 1001):
+            if i + j > 1000:
+                break
             for k in range(j+1, 1001):
+                    if i+j < k:
+                        break
+                    
                     if i + j + k > 1000:
                         continue
                     
                     if i*i + j*j == k*k:
                         my_dict[i + j + k] += 1
-                
-    print(max(my_dict, key=my_dict.get))
+                        # ans = max(ans, my_dict[i + j + k])
 
+    print(max(my_dict, key=my_dict.get))
 
 import time
 
