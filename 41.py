b3bd38b821abc75c883aeb5de03130abb75bd4a6
Siddharth Velankar <Siddharth.Velankar@dell.com>
2025-01-22 15:26:36 -0500

    Solved 41

---
+++ 41.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from itertools import permutations
+num = '7654321'
+
+import time
+def solve():
+    import sympy
+    for i in permutations(num):
+        perm = int(''.join(i))
+        if sympy.isprime(perm):
+            print(perm)
+            break
+
+start_time = time.time()
+solve()
+end_time = time.time()
+
+print("Time taken: %s seconds" % (end_time - start_time))
