7581d4e63d9d17652588a9fa0641e734f8d077d6
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-03-04 20:36:44 -0500

    Solved 30

---
+++ 30.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from itertools import count
+
+from mytimeit import *
+import time
+
+
+def solve():
+    import math
+
+    ans = 0
+    for i in range(10, 1000000):
+        x = i
+        sum = 0
+        while x != 0:
+            rem = x % 10
+            x //= 10
+            sum += math.pow(rem, 5)
+
+        if sum == i:
+            print(sum)
+            ans += sum
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    with MyTimer(solve) as timer:
+        solve()
