f1aa6889fef31e7ef85a5f77e4e7917548642f1c
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-03-10 18:21:15 -0400

    Solved 58

---
+++ 58.py
@@ -0,0 +1,35 @@
+
+#!/usr/bin/env python3
+
+from mytimeit import *
+import time
+from sympy import isprime
+import math
+
+
+def solve():
+    x = 2
+    num = 1
+    cnt = 0
+    total_cnt = 1
+    ans = 1
+    while True:
+        for i in range(4):
+            num += x
+            total_cnt += 1
+            if isprime(num):
+                cnt += 1
+
+        ans += 1
+        x += 2
+        
+        # print(cnt, total_cnt, cnt/total_cnt, num)
+        # time.sleep(5)
+        if cnt/total_cnt < 0.1:
+            print(math.sqrt(num))
+            break
+
+
+if __name__ == "__main__":
+    with MyTimer(solve) as timer:
+        solve()
