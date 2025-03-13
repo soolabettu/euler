244f3f42be212529edcc800c7303b37aab19c8ad
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-03-13 18:58:14 -0400

    Solved 57

--- 57.py
+++ 57.py
@@ -10,14 +10,12 @@ from fractions import Fraction
 def solve():
 
     f1 = Fraction(2, 1)
+    f2 = Fraction(2, 1)
     ans = 0
     for i in range(1000):
-        f2 = Fraction(2, 1)
-        for j in range(i+1):
-            f2 = f1 + 1/f2
-
-        f2 = 1 + 1/f2
-        ans += len(str(f2.numerator)) > len(str(f2.denominator))    
+        f2 = f1 + 1/f2
+        f3 = 1 + 1/f2
+        ans += len(str(f3.numerator)) > len(str(f3.denominator))    
         
     print(ans)
 
