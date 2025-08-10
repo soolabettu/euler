0896bf1207288ffb3af106fe08f47390cee1de44
soolabettu <17737361+soolabettu@users.noreply.github.com>
2025-08-10 17:50:02 -0400

    Solved 38.py

---
+++ 38.py
@@ -0,0 +1,22 @@
+import mytimeit
+
+
+def solve() -> int:
+    max_prod = 0
+    for i in range(99999, 1, -1):
+        prod = ""
+        for j in range(1, 10):
+            prod += str(i * j)
+            if len(prod) > 9:
+                break
+
+            if len(prod) == 9:
+                sorted_prod = sorted(prod, key=int)
+                if "".join(sorted_prod) == "123456789":
+                    max_prod = max(int(prod), max_prod)
+
+    return max_prod
+
+
+with mytimeit.MyTimer() as t:
+    print(solve())
