ffb079dbb2dcd8b0e4fc4749cfd958b9f3e3ec7b
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-08-31 06:51:18 -0400

    Solved 99

---
+++ 99.py
@@ -0,0 +1,20 @@
+import mytimeit
+from math import log10
+
+
+def solve():
+    max_number = 0
+    max_line = 0
+    with open("0099_base_exp.txt", "r") as f:
+        for i, line in enumerate(f):
+            base, exp = map(int, line.split(","))
+            number = exp * log10(base)
+            if number > max_number:
+                max_number = number
+                max_line = i + 1
+
+    return max_line
+
+
+with mytimeit.MyTimer() as t:
+    print(solve())
