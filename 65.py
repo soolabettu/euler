c9c2d0783aea9b27e409d31c032f58c9992895f3
soolabettu <17737361+soolabettu@users.noreply.github.com>
2025-09-02 13:03:25 -0400

    Solved 65

---
+++ 65.py
@@ -0,0 +1,35 @@
+import mytimeit
+
+
+import math
+
+
+def continued_fraction_period(n):
+    p2, p1, q2, q1, a = 0, 1, 1, 0, 2
+
+    k = 1
+    x = 1
+    for i in range(n):
+        p1, p2, q1, q2 = a * p1 + p2, p1, a * q1 + q2, q1
+        # print(p1, q1)
+        if k % 3 == 1:
+            a = 1
+        elif k % 3 == 2:
+            a = 2 * x
+            x += 1
+        else:
+            a = 1
+
+        k += 1
+
+    return sum([int(c) for c in str(p1)])
+
+
+def solve(n):
+    return continued_fraction_period(n)
+
+
+import mytimeit
+
+with mytimeit.MyTimer() as t:
+    print(solve(100))
