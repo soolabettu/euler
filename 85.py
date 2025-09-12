2c8a75034bc432bbb1d3c04412cebbe0b86b3ea2
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-09-12 16:52:55 -0400

    Solved 85

---
+++ 85.py
@@ -0,0 +1,32 @@
+import mytimeit
+
+
+import math
+
+
+def solve(limit):
+    margin = 3000000
+    area = 0
+    nearest_dim = (0, 0)
+    for i in range(1, limit):
+        for j in range(1, i):
+            part1 = math.factorial(i + 1) // (
+                math.factorial(i + 1 - 2) * math.factorial(2)
+            )
+            part2 = math.factorial(j + 1) // (
+                math.factorial(j + 1 - 2) * math.factorial(2)
+            )
+            rectangles = part1 * part2
+            diff = abs(rectangles - 2000000)
+            if diff < margin:
+                margin = diff
+                area = i * j
+                nearest_dim = (i, j)
+
+    return (area, nearest_dim)
+
+
+import mytimeit
+
+with mytimeit.MyTimer() as t:
+    print(solve(100))
