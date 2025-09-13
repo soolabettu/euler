6348eceea62a96b91325a44e67e28f3efbe04ac3
Siddharth Velankar <17737361+soolabettu@users.noreply.github.com>
2025-09-13 11:30:07 -0400

    Solved 95

---
+++ 95.py
@@ -0,0 +1,35 @@
+import sympy
+from collections import defaultdict
+
+
+import math
+
+
+# Example usage
+def solve(limit):
+    a = [1] * limit
+    for i in range(2, limit):
+        for j in range(2 * i, limit, i):
+            a[j] += i
+
+    max_chain = []
+    for i in range(1, limit):
+        num = i
+        seen = []
+        while num < limit:
+            if num in seen:
+                if num == i:
+                    if len(seen) > len(max_chain):
+                        max_chain = seen
+                break
+
+            seen.append(num)
+            num = a[num]
+
+    return max_chain
+
+
+from mytimeit import *
+
+with MyTimer() as t:
+    print(solve(1000000))
