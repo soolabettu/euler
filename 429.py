c9ef8cc151d394747251fcb64f5fb6ebde3f39c9
soolabettu <17737361+soolabettu@users.noreply.github.com>
2025-09-23 16:58:21 -0400

    Update 429.py

--- 429.py
+++ 429.py
@@ -1,26 +1,20 @@
-i = 90
-j = 3
-check_str = "123"
-k = 1
-limit = 678910
-n = 13
-p = int(str(2**90)[:n])
-p196 = int(str(2**196)[:n])
-p289 = int(str(2**289)[:n])
-p485 = int(str(2**485)[:n])
+def solve(limit):
+    i = 0
+    x = 1
+    k = 0
+    while i < limit:
+        x *= 2
+        k += 1
+        if x > 1000:
+            x /= 10
+
+        if int(x) == 123:
+            i += 1
+
+    return k
 
 
-while k < limit:
-    if str(p*p196)[:j] == check_str:
-        i = i + 196
-        p = int(str(p*p196)[:n])
-       
-    elif str(p*p289)[:j] == check_str:
-        i = i + 289
-        p = int(str(p*p289)[:n])
-        
-    elif str(p*p485)[:j] == check_str:
-        i = i + 485
-        p = int(str(p*p485)[:n])
-        
-    k = k + 1
+from mytimeit import MyTimer
+
+with MyTimer() as t:
+    print(solve(678910))
