6220497f6ff77d0e0c75c0a39aafde0faaf71f7f
Siddharth Velankar <soolabettu@users.noreply.github.com>
2026-04-19 08:10:54 -0400

    Add formatted solution for Euler 868

---
+++ 868.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+import sys
+
+inv_cnt = defaultdict(int)
+char_to_digit = {}
+
+
+def rank_characters(s):
+    return {c: i for i, c in enumerate(sorted(set(s)), start=1)}
+
+
+def inversion_count(s):
+    global char_to_digit
+    char_to_digit = rank_characters(s)
+    digits = [char_to_digit[c] for c in s]
+    inv_cnt.clear()
+    inv_cnt.update(
+        (digit, sum(d < digit for d in digits[i + 1 :]))
+        for i, digit in enumerate(digits)
+    )
+    return digits, inv_cnt
+
+
+def solve(values, digit, count, digit_totals):
+    if digit == len(values) + 1:
+        print(digit_totals[-1])
+        return
+
+    current_digit_moves = (digit - 1) * count
+    less_than = inv_cnt[digit] if count % 2 == 0 else digit - inv_cnt[digit] - 1
+    next_count = count + current_digit_moves + less_than
+    return solve(values, digit + 1, next_count, digit_totals + [next_count])
+
+
+if __name__ == "__main__":
+    s = sys.argv[1] if len(sys.argv) > 1 else "NOWPICKBELFRYMATHS"
+    digits, _ = inversion_count(s)
+    solve(digits, 3, inv_cnt[2], [])
