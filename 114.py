e89123935c220b4b220946c849facdd10afb055a
Siddharth Velankar <soolabettu@users.noreply.github.com>
2026-05-03 08:29:19 -0400

    Pythonize Euler 114 solution

--- 114.py
+++ 114.py
@@ -1,32 +1,26 @@
 from functools import lru_cache
 
-cache = {}
 
-
+@lru_cache(maxsize=None)
 def solve(current_sum, prev_state):
-    if (prev_state, current_sum) in cache:
-        return cache[(prev_state, current_sum)]
-
     if current_sum > n:
         return 0
+
     if current_sum == n:
         return 1
 
-    total = 0
     if prev_state == 0:
-        for length in range(3, n - current_sum + 1):
-            total += solve(current_sum + length, 1)
-    else:
-        for length in range(1, n - current_sum + 1):
-            total += solve(current_sum + length, 0)
+        return sum(
+            solve(current_sum + length, 1)
+            for length in range(3, n - current_sum + 1)
+        )
 
-    cache[(prev_state, current_sum)] = total
-    return total
+    return sum(
+        solve(current_sum + length, 0)
+        for length in range(1, n - current_sum + 1)
+    )
 
 
 if __name__ == "__main__":
     n = 50
-    total = 0
-    for i in range(n + 1):
-        total += solve(i, 0)
-    print(total)
+    print(sum(solve(i, 0) for i in range(n + 1)))
